2f3538376a73d85c18d65a517a9de53e642da19a
valeria campilongo <cvaleriac@hotmail.it>
2019-06-10 18:47:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,43 @@
+let store = { drivers: [], passengers: [], trips:[] };
+// initialize store with key of items and users that each point to an empty array
+
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+
+class Driver {
+    constructor(name) {
+        this.id = ++driverId;
+        this.name = name;
+
+        store.drivers.push(this);
+    }
+    trips() {
+        return store.trips.filter(
+            function(trip) {
+                return trip.driverId === this.id;
+            }.bind(this)
+        );
+    }
+    passengers() {
+        return store.passengers.filter(
+            function(passenger) {
+                return passenger.tripId === this.tripId;
+            }.bind(this)
+        );
+    }
+}
+
+
+class Passenger {
+    constructor(name, price, user) {
+        this.id = ++itemId;
+        this.name = name;
+        }
+
+        store.passenger.push(this);
+    }
+    setUser(driver) {
+        this.driverId = driver.id;
+    }
+}
